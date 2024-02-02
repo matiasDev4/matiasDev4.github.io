806777da4aaa4f65164390dc762b08d46cde4aa0
Matias alvarez <107509747+matiasDev4@users.noreply.github.com>
2024-02-01 23:26:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const cerrar = document.getElementById('cerrar');
 const nav = document.getElementById('nav');
 const abrir = document.getElementById('abrir')
+const copiado = document.getElementById('copiado')
 
 cerrar.addEventListener('click', ()=>{
     nav.classList.toggle('mobile:right-[100%]')
+    
 })
 abrir.addEventListener('click', ()=>{
     nav.classList.remove('mobile:right-[100%]')
@@ -11,4 +13,21 @@ abrir.addEventListener('click', ()=>{
 
 function link(){
     nav.classList.toggle('mobile:right-[100%]')
+}
+function copiar(){
+    navigator.clipboard.writeText('matiasalvarezinfo1@gmail.com')
+    copiado.classList.remove('hidden')
+    copiado.animate([{
+        opacity:1,
+        transform:"translateY(0)"
+    },
+    {
+        opacity:0.25,
+        transform:"translateY(-8px)"
+    }],{
+        duration:1000,
+    })
+    setTimeout(()=>{
+        copiado.classList.toggle('hidden')
+    },1000)
 }
